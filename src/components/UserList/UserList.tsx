e4c30257ad20eb07a12878e258b5d2750cc104f4
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-05-24 16:21:47 +0200

    added interface User to UserList

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -2,53 +2,46 @@ import Navigation from "../Navigation/Navigation";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-// interface User {
-//     id: number,
-//     firstName: string,
-//     lastName: string;
-// }
+// Define the User interface
+interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+}
 
 const UserList = () => {
-
-    const [users, setUsers] = useState("");
+    // Initialize users with an empty array of type User
+    const [users, setUsers] = useState<User[]>([]);
 
     const fetchUserList = async () => {
-
         try {
-            const response = await fetch ('https://dummyjson.com/users');
+            const response = await fetch('https://dummyjson.com/users');
 
             if (!response.ok) throw new Error("Something goes wrong!");
 
-            const users = await response.json();
+            const data = await response.json();
 
-            setUsers(users);
-         
-        }
-
-         catch (error) {
+            setUsers(data.users); // Update this line to set the correct part of the response
+        } catch (error) {
             console.error("Error fetching users:", error);
-          }
+        }
     }
 
     useEffect(() => {
         fetchUserList();
-      }, []);
+    }, []);
 
     return (
-
         <div>
-            
-      <Navigation />
+            <Navigation />
             <h1>User List</h1>
             <ul className="users">
                 {users.map((user) => (
                     <li key={user.id}>
-                        <Link to={`/user-details/${user.id}`}>{user.firstName} {user.lastName}</Link>
+                        <Link to={`/userDetails/${user.id}`}>{user.firstName} {user.lastName}</Link>
                     </li>
                 ))}
-
-                </ul>
-     
+            </ul>
         </div>
     );
 }
